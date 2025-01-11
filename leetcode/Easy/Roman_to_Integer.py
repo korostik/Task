692126d4b2b667da36fe453a9a49595993991189
korostik <andreykorostik@yandex.ru>
2025-01-11 11:28:41 +0300

    https://leetcode.com/problems/roman-to-integer/

---
+++ leetcode/Easy/Roman_to_Integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        s += "O"
+        d = {}
+        d["O"] = 0
+        d["I"] = 1
+        d["V"] = 5
+        d["X"] = 10
+        d["L"] = 50
+        d["C"] = 100
+        d["D"] = 500
+        d["M"] = 1000
+        i = 0
+        count = 0
+        while i != len(s) - 1:
+            if d[s[i]] >= d[s[i + 1]]:
+                count += d[s[i]]
+                i += 1
+            else:
+                count += d[s[i + 1]] - d[s[i]]
+                i += 2
+        return count
+
+        
