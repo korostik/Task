98804300f465ae1d8dc10338f776cceba94ace16
korostik <andreykorostik@yandex.ru>
2025-01-19 23:37:52 +0300

    https://leetcode.com/problems/plus-one/

---
+++ leetcode/Easy/plus_one.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def plusOne(self, a):
+        a = int(''.join(map(str, a)))
+        a = a + 1
+        ans = []
+        while a > 0:
+            ans.append(a % 10)
+            a //= 10
+        return ans[::-1]
+
+
+        
