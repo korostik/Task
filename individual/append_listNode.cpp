6cfe69793f10f842e7e36fcf04611972fe6702d6
korostik <andreykorostik@yandex.ru>
2025-02-12 21:08:30 +0300

    добавление элемента в связный список

---
+++ individual/append_listNode.cpp
@@ -0,0 +1,17 @@
+ListNode* append(ListNode *node, int value){
+
+    ListNode* Newnode = new ListNode;
+    Newnode->val = value;
+    Newnode->next = nullptr;
+    if (node == nullptr){
+        return Newnode;
+    }else{
+        ListNode* temp = node;
+        while (temp->next != nullptr){
+            temp = temp->next;
+        }
+        temp->next = Newnode;
+
+    }
+    return node;
+}
