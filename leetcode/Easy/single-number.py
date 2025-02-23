456a6af7f262b0572b0376b8d1bb43c9a18f1ff5
korostik <andreykorostik@yandex.ru>
2025-02-23 15:07:48 +0300

    https://leetcode.com/problems/single-number/

---
+++ leetcode/Easy/single-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def singleNumber(self, a):
+        d = {}
+        for i in range(len(a)):
+            d[a[i]] = d.get(a[i], 0) + 1
+        for k, v in d.items():
+            if v == 1:
+                return k
+
+                
