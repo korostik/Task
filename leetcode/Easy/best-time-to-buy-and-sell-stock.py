4ffab761237d3cd812adad75e97a4f496ef535c8
korostik <andreykorostik@yandex.ru>
2025-04-04 17:35:25 +0300

    a day to buy and other day in the future to sell to make as much profit as possible

--- leetcode/Easy/best-time-to-buy-and-sell-stock.py
+++ leetcode/Easy/best-time-to-buy-and-sell-stock.py
@@ -1,3 +1,4 @@
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 class Solution(object):
     def maxProfit(self, a):
         ans = 0
@@ -6,22 +7,20 @@ class Solution(object):
         ind_mini = 0
         ind_maxi = 0
         for i in range(len(a)):
-            x = a[i]
             if ind_mini < ind_maxi and i != 0:
                 ans = max(ans, a[ind_maxi] - a[ind_mini])
             elif ind_mini > ind_maxi:
                 ind_maxi = -1
                 maxi = -1
-            if maxi < x:
-                maxi = x
+            if maxi < a[i]:
+                maxi = a[i]
                 ind_maxi = i
-            if mini > x:
-                mini = x
+            if mini > a[i]:
+                mini = a[i]
                 ind_mini = i
             
         if ind_mini < ind_maxi and i != 0:
             ans = max(ans, a[ind_maxi] - a[ind_mini])
         return ans
-
 
 
