fbdb58e159242bdadb665877d4a92288dc9f0236
korostik <andreykorostik@yandex.ru>
2025-05-16 15:19:33 +0300

    Update and rename 58.py to Length of Last Word № 58.py

--- leetcode/Length of Last Word № 58.py
+++ leetcode/Length of Last Word № 58.py
@@ -1,5 +1,6 @@
 
-#https://leetcode.com/problems/length-of-last-word/description/class Solution(object):
+#https://leetcode.com/problems/length-of-last-word/description/
+class Solution(object):
     def lengthOfLastWord(self, s):
         s = s.lstrip().rstrip()
         k = s.rfind(" ")
