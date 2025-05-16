880fb1c52ddbbf0f03d17ff16fcc6b38759d3beb
korostik <andreykorostik@yandex.ru>
2025-05-16 11:29:35 +0300

    Update and rename mergesort.cpp to 21.cpp

--- leetcode/21.cpp
+++ leetcode/21.cpp
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/merge-two-sorted-lists/description/
 ListNode* append(ListNode *node, int value){
 
     ListNode* Newnode = new ListNode;
@@ -66,5 +67,6 @@ public:
         return ans;
     }
 };
+
 
 
