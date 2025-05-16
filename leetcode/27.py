b4b52986f08c075a5443b6bda708f67f5342bc52
korostik <andreykorostik@yandex.ru>
2025-05-16 11:14:03 +0300

    Update and rename Remove_Element.py to 27.py

--- leetcode/27.py
+++ leetcode/27.py
@@ -1,8 +1,7 @@
+#https://leetcode.com/problems/remove-element/description/
 class Solution(object):
     def removeElement(self, nums, val):
         while val in nums:
             nums.remove(val)
         return len(nums)
-        
-
         
