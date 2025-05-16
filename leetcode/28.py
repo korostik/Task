8baac71e9cf72e88204778a33c2bf4a7d1fb488e
korostik <andreykorostik@yandex.ru>
2025-05-16 11:26:31 +0300

    Update and rename find_the_index_of_the_first_occurrence_in_a_string.py to 28.py

--- leetcode/28.py
+++ leetcode/28.py
@@ -1,3 +1,4 @@
+#https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 class Solution(object):
     def strStr(self, haystack, needle):
         if needle in haystack:
