250fd15160f0158a130502bb01420b646293df02
korostik <andreykorostik@yandex.ru>
2025-05-16 11:27:43 +0300

    Update and rename length_of_last_word.py to 58.py

--- leetcode/58.py
+++ leetcode/58.py
@@ -1,8 +1,7 @@
-class Solution(object):
+
+#https://leetcode.com/problems/length-of-last-word/description/class Solution(object):
     def lengthOfLastWord(self, s):
         s = s.lstrip().rstrip()
         k = s.rfind(" ")
         return len(s[k + 1:])
-        
-
         
