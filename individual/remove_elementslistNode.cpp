f975011da6973222d66b9999b5f49829992f626f
korostik <andreykorostik@yandex.ru>
2025-06-06 10:03:11 +0000

    сделал

--- individual/remove_elementslistNode.cpp
+++ individual/remove_elementslistNode.cpp
@@ -1,18 +1,21 @@
+//https://leetcode.com/problems/remove-linked-list-elements/solutions/902295/simple-recusrion/
+
 class Solution {
 public:
     ListNode* removeElements(ListNode* node, int val) {
         ListNode* temp = node;
         int k;
-        
+
         while (temp != nullptr) {
             k = 0;
             if (temp->next != nullptr){
                 if (temp->next->val == val){
                     temp->next = temp->next->next;
-                    k = 1;
+                    
+                }else{
+                    temp = temp->next;
                 }
             }
-            if (k == 0){temp = temp->next;}
         }
         if (node != nullptr)
             if (node->val == val)return node->next;
