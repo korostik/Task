a44ab540fe8e840299dcdf3fd2d4cd2208cbf86d
korostik <andreykorostik@yandex.ru>
2025-06-20 09:13:50 +0300

    Update 209.MinimumSizeSubarraySum.py

--- leetcode/209.MinimumSizeSubarraySum.py
+++ leetcode/209.MinimumSizeSubarraySum.py
@@ -7,18 +7,15 @@ class Solution(object):
         now_sum = 0
         minilen = 10**10
         for i in range(len(a)):
-            if now_sum + a[i] < x:
-                now_sum += a[i]
-                right += 1
-            else:
+            now_sum += a[i]
+            if now_sum >= x:
                 sum_left = a[left]
-                now_sum += a[i]
                 while now_sum - sum_left >= x:
                     now_sum -= a[left]
                     left += 1
                     sum_left = a[left]
                 minilen = min(minilen, right - left + 1)
-                right += 1
+            right += 1
                 
         if sum(a) < x:
             return 0
