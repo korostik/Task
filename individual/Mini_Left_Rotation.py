d6121ecbb8d294d50202d37b9bd7c5f890478beb
korostik <andreykorostik@yandex.ru>
2025-11-05 15:01:20 +0000

    Малое левое вращение. Данное вращение используется тогда, когда разница высот L-поддерева и b-поддерева равна 2 и высота С <= высота R.

---
+++ individual/Mini_Left_Rotation.py
@@ -0,0 +1,27 @@
+# Данное вращение используется тогда, когда разница высот L-поддерева и b-поддерева равна 2 и высота С <= высота R.
+
+# class Node:
+#     def __init__(self, value):
+#         self.right = None
+#         self.left = None
+#         self.val = value
+
+
+
+def Rotation(tree): # текущий узел основного дерева, который подходит под условие балансировки малым левым вращением
+    b_left = tree.right.left
+    tree.right.left = None
+    a_right = tree.right
+    tree.right = None
+    tree.right = b_left
+    a_right.left = tree
+    return a_right
+
+    
+
+# tree = Node("a")
+# tree.left = Node("L")
+# tree.right = Node("b")
+# tree.right.left = Node("C")
+# tree.right.right = Node("R")
+# tree = Rotation(tree)
