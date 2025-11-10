125cd6d02bb2a564474b4918bd01e81294db50c4
korostik <andreykorostik@yandex.ru>
2025-11-10 15:13:17 +0000

    right rotation Данное вращение используется тогда, когда разница высот R-поддерева и b-поддерева равна 2 и высота С <= высота L.

---
+++ individual/Mini_Right_Rotation.py
@@ -0,0 +1,29 @@
+# Данное вращение используется тогда, когда разница высот R-поддерева и b-поддерева равна 2 и высота С <= высота L.
+# class Node:
+#     def __init__(self, value):
+#         self.right = None
+#         self.left = None
+#         self.val = value
+
+
+
+def Rotation(tree): # текущий узел основного дерева, который подходит под условие балансировки малым левым вращением
+    a_left = tree.left #b
+    tree.left = None
+    b_right = a_left.right
+    a_left.right = None
+    tree.left = b_right
+    a_left.right = tree
+    return a_left
+
+
+
+
+    
+
+# tree = Node("a")
+# tree.left = Node("b")
+# tree.right = Node("R")
+# tree.left.right = Node("C")
+# tree.left.left = Node("L")
+# tree = Rotation(tree)
