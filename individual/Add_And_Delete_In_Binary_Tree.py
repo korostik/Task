e5cc0ae362b718836a52a4d216dd87f2d6efbe7c
korostik <andreykorostik@yandex.ru>
2025-11-18 19:38:55 +0000

    1)do find_min_in_right_subtree 2)update Add_And_Delete_In_Binary_Tree

--- individual/Add_And_Delete_In_Binary_Tree.py
+++ individual/Add_And_Delete_In_Binary_Tree.py
@@ -1,3 +1,10 @@
+# В онлайн-игре ведется таблица рейтинга игроков. Каждому игроку начисляются очки. Необходимо реализовать систему, которая должна обрабатывать три типа запросов:
+# 1.  Добавить нового игрока с его рейтингом (или обновить рейтинг существующего игрока).
+# 2.  Удалить игрока из таблицы.
+# 3.  Найти место (ранг) игрока в таблице рейтинга. Место 1 — у игрока с наивысшим рейтингом.
+
+
+
 class Node:
     def __init__(self, value):
         self.right = None
@@ -6,6 +13,8 @@ class Node:
     
 def Add(tree, value_new):
     temp = tree
+    if tree == None:
+        return Node(value_new)
     while True:
         if value_new > temp.val:
             if temp.right:
@@ -27,70 +36,57 @@ def Add(tree, value_new):
 def delete(tree, value):
     #найти то что удаляем
     temp = tree
-    if temp.val != value:
-        while True:
-            k = temp.val
-            if temp.right != None and value > temp.right.val:
-                if temp.right:
-                    temp = temp.right
-                else:
-                    break
-            elif temp.left != None and value < temp.left.val:
-                if temp.left:
-                    temp = temp.left
-                else:
-                    break
+    while temp:
+        if temp != None and value > temp.val:
+            temp_before_val = temp
+            temp = temp.right
+        elif value < temp.val:
+            temp_before_val = temp
+            temp = temp.left
+        else:
+            if temp_before_val.left and temp_before_val.left.val == value:
+                napr = 'left'
             else:
-                if temp.left and temp.left.val == value:
-                    napr = 'left'
-                else:
-                    napr = 'right'
-                break
+                napr = 'right'
+            break
+    
+    
     #если два ребенка Null, то делаем этот узел Null
     if napr == 'right':
-        if temp.right.right == None and temp.right.left == None:
-            temp.right = None
-            return temp
+        if temp_before_val.right.right == None and temp_before_val.right.left == None:
+            temp_before_val.right = None
+            return tree
     if napr == 'left':
-        if temp.left.right == None and temp.left.left == None:
-            temp.left = None
-            return temp
+        if temp_before_val.left.right == None and temp_before_val.left.left == None:
+            temp_before_val.left = None
+            return tree
     
     #если один ребенок Null, то передвигаем ребенка на место того, кого мы должны удалить
     if napr == 'right':
-        if temp.right.right == None and temp.right.left != None:
-            n = temp.right.left
-            temp.right.left = None
-            temp.right = n
-            return temp
-        if temp.right.right != None and temp.right.left == None:
-            n = temp.right.right
-            temp.right.right = None
-            temp.right = n
-            return temp
+        if temp_before_val.right.right == None and temp_before_val.right.left != None:
+            n = temp_before_val.right.left
+            temp_before_val.right.left = None
+            temp_before_val.right = n
+            return tree
+        if temp_before_val.right.right != None and temp_before_val.right.left == None:
+            n = temp_before_val.right.right
+            temp_before_val.right.right = None
+            temp_before_val.right = n
+            return tree
         
     if napr == 'left':
-        if temp.left.right == None and temp.left.left != None:
-            n = temp.left.left
-            temp.left.left = None
-            temp.left = n
-            return temp
+        if temp_before_val.left.right == None and temp_before_val.left.left != None:
+            n = temp_before_val.left.left
+            temp_before_val.left.left = None
+            temp_before_val.left = n
+            return tree
         
-        if temp.left.right != None and temp.left.left == None:
-            n = temp.left.right
-            temp.left.right = None
-            temp.left = n
-            return temp
+        if temp_before_val.left.right != None and temp_before_val.left.left == None:
+            n = temp_before_val.left.right
+            temp_before_val.left.right = None
+            temp_before_val.left = n
+            return tree
         
     #находим в правом дереве самый левый элемент и переставляем его ребенка(если он есть) на место родителя а родителя на место удаляемого элемента
     
 
-
-
-
-tree = Node(6)
-tree.left = Node(5)
-tree.right = Node(7)
-tree.left.right = Node(8)
-tree = delete(tree, 5)
-print(tree.left.val)
