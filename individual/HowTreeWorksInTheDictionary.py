18e26c1e0385aeea23d7b000f15d8472304f7c89
korostik <andreykorostik@yandex.ru>
2026-01-10 09:04:59 +0000

    Change attributes in the class.

--- individual/HowTreeWorksInTheDictionary.py
+++ individual/HowTreeWorksInTheDictionary.py
@@ -1,15 +1,16 @@
-class Tree:
-    def __init__(self, key, value=1):
+class Dictionary:
+    def __init__(self, key, value):
         self.right = None
         self.left = None
-        self.val = [key, value]
+        self.key = key
+        self.value = value
 
 def Add_and_Count(tree, key_new):
     temp = tree
     if tree == None:
-        return Tree(key_new)
+        return Dictionary(key_new, 1)
     while True:
-        if key_new > temp.val[0]:
+        if key_new > temp.key:
             if temp.right:
                 temp = temp.right
             else:
@@ -20,32 +21,32 @@ def Add_and_Count(tree, key_new):
             else:
                 break
 
-    if key_new > temp.val[0]:
-        temp.right = Tree(key_new)
-    elif key_new < temp.val[0]:
-        temp.left = Tree(key_new)
+    if key_new > temp.key:
+        temp.right = Dictionary(key_new, 1)
+    elif key_new < temp.key:
+        temp.left = Dictionary(key_new, 1)
     else:
-        temp.val[1] += 1
+        temp.value += 1
     return tree
 
-def get(tree, key):
+def get(tree, key_):
     temp = tree
     while temp:
-        if temp.val[0] < key:
+        if temp.key < key_:
             temp = temp.right
-        elif temp.val[0] > key:
+        elif temp.key > key_:
             temp = temp.left
         else:
-            return temp.val[1]
+            return temp.value
     return "ERROR"
 
 
-# n = int(input())
-# tree = None
-# a = []
-# for i in range(n):
-#     print(a)
-#     x, y = map(int, input().split())
-#     a.append(x)
-#     tree = Add_and_Count(tree, x)
-#     print(get(tree, y))
+n = int(input())
+tree = None
+a = []
+for i in range(n):
+    print(a)
+    x, y = map(int, input().split())
+    a.append(x)
+    tree = Add_and_Count(tree, x)
+    print(get(tree, y))
