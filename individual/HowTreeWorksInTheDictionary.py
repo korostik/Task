6fbc475888e2186daa042441d537280cffb7b9e5
korostik <andreykorostik@yandex.ru>
2026-01-10 09:13:17 +0000

    Change attributes in class.

--- individual/HowTreeWorksInTheDictionary.py
+++ individual/HowTreeWorksInTheDictionary.py
@@ -5,48 +5,49 @@ class Dictionary:
         self.key = key
         self.value = value
 
-def Add_and_Count(tree, key_new):
-    temp = tree
-    if tree == None:
-        return Dictionary(key_new, 1)
-    while True:
-        if key_new > temp.key:
-            if temp.right:
-                temp = temp.right
+    def Add_and_Count(tree, key_new):
+        temp = tree
+        if tree == None:
+            return Dictionary(key_new, 1)
+        while True:
+            if key_new > temp.key:
+                if temp.right:
+                    temp = temp.right
+                else:
+                    break
             else:
-                break
+                if temp.left:
+                    temp = temp.left
+                else:
+                    break
+
+        if key_new > temp.key:
+            temp.right = Dictionary(key_new, 1)
+        elif key_new < temp.key:
+            temp.left = Dictionary(key_new, 1)
         else:
-            if temp.left:
+            temp.value += 1
+        return tree
+
+    def get(tree, key_):
+        temp = tree
+        while temp:
+            if temp.key < key_:
+                temp = temp.right
+            elif temp.key > key_:
                 temp = temp.left
             else:
-                break
-
-    if key_new > temp.key:
-        temp.right = Dictionary(key_new, 1)
-    elif key_new < temp.key:
-        temp.left = Dictionary(key_new, 1)
-    else:
-        temp.value += 1
-    return tree
-
-def get(tree, key_):
-    temp = tree
-    while temp:
-        if temp.key < key_:
-            temp = temp.right
-        elif temp.key > key_:
-            temp = temp.left
-        else:
-            return temp.value
-    return "ERROR"
+                return temp.value
+        return "ERROR"
 
 
-n = int(input())
-tree = None
-a = []
-for i in range(n):
-    print(a)
-    x, y = map(int, input().split())
-    a.append(x)
-    tree = Add_and_Count(tree, x)
-    print(get(tree, y))
+# n = int(input())
+# tree = None
+# obj = Dictionary
+# a = []
+# for i in range(n):
+#     print(a)
+#     x, y = map(int, input().split())
+#     a.append(x)
+#     tree = obj.Add_and_Count(tree, x)
+#     print(obj.get(tree, y))
