38c93687fb49a52dc78c7e13dd51bc202527b2a5
korostik <andreykorostik@yandex.ru>
2026-01-12 21:49:40 +0000

    Добавление, удаление элементов из бинарного дерева.

---
+++ individual/Add_And_Delete_In_Binary_Tree.py
@@ -0,0 +1,108 @@
+# В онлайн-игре ведется таблица рейтинга игроков. Каждому игроку начисляются очки. Необходимо реализовать систему, которая должна обрабатывать три типа запросов:
+# 1.  Добавить нового игрока с его рейтингом (или обновить рейтинг существующего игрока).
+# 2.  Удалить игрока из таблицы.
+# 3.  Найти место (ранг) игрока в таблице рейтинга. Место 1 — у игрока с наивысшим рейтингом.
+
+class Tree:
+    def __init__(self, value):
+        self.right = None
+        self.left = None
+        self.val = value
+    
+    def Add(tree, value_new):
+        temp = tree
+        if tree == None:
+            return Tree(value_new)
+        while True:
+            if value_new > temp.val:
+                if temp.right:
+                    temp = temp.right
+                else:
+                    break
+            else:
+                if temp.left:
+                    temp = temp.left
+                else:
+                    break
+        if value_new > temp.val:
+            temp.right = Tree(value_new)
+        else:
+            temp.left = Tree(value_new)
+        return tree
+        
+    def find_root_minelement_in_right_tree(self, temp):
+        ans = temp
+        pred = None
+        while temp:
+            if temp.left:
+                pred = temp
+                ans = temp.left
+                temp = temp.left
+            else:
+                temp = temp.right
+        return ans, pred
+
+    def delete(self, tree2, value):
+        k = 1
+        if tree2.val == value:
+            tree = Tree(-10**9)
+            tree.right = tree2
+            k = 0
+        else:
+            tree = tree2
+        #найти то что удаляем
+        temp = tree
+        while temp:
+            h = temp.val
+            if temp != None and value > temp.val:
+                temp_before_val = temp
+                temp = temp.right
+            elif value < temp.val:
+                temp_before_val = temp
+                temp = temp.left
+            else:
+                if temp_before_val.left and temp_before_val.left.val == value:
+                    napr = temp_before_val.left
+                else:
+                    napr = temp_before_val.right
+                break
+            
+        #napr указывает на удаляемый элемент
+
+        # удаление листа
+        if napr.left == None and napr.right == None:
+            if temp_before_val.left == napr:
+                temp_before_val.left = None
+            else:
+                temp_before_val.right = None
+        
+        # удаление узла с одним ребенком
+        if napr.left == None and napr.right != None or napr.left != None and napr.right == None:
+            if temp_before_val.left == napr:
+                if napr.left != None:
+                    temp_before_val.left = napr.left
+                else:
+                    temp_before_val.left = napr.right
+            else:
+                if napr.left != None:
+                    temp_before_val.right = napr.left
+                else:
+                    temp_before_val.right = napr.right
+
+        # удаление узла с двумя детьми
+        if napr.left != None and napr.right != None:
+            minelement_in_right_tree, pred = self.find_root_minelement_in_right_tree(napr.right)
+            napr.val = minelement_in_right_tree.val
+            if pred == None: #нет самого левого элемента в правом дереве
+                napr.val = minelement_in_right_tree.val
+                napr.right = napr.right.right
+            elif pred.left.right != None:
+                pred.left = pred.left.right
+        if k == 0:
+            return tree.right
+        return tree
+
+# tree = Tree(5)
+# tree = tree.Add(4)
+# tree = tree.delete(tree, 5)
+# print(tree.val)
